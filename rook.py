750ec68e97f1e00122a172b26db3df3711dfabb5
rakahadhyana <hadhyana@gmail.com>
2018-09-14 19:58:27 +0700

    Rook Eat

--- rook.py
+++ rook.py
@@ -15,36 +15,53 @@ def rookEat(rook, state):
       up.append(catur)
     else:
       down.append(catur)
-  print(horizontal)
   # Search nearest Up
   if len(up) > 0:
     nearestUp = up[0]
     for catur in up:
       if catur["y"] - rook["y"] < nearestUp["y"] - rook["y"]:
         nearestUp = catur
-  if nearestUp["color"] != rook["color"]:
-    count += 1
+    if nearestUp["color"] != rook["color"]:
+      count += 1
   # Search nearest Down
   if len(down) > 0:
     nearestDown = down[0]
     for catur in down:
       if rook["y"] - catur["y"] < rook["y"] - nearestDown["y"]:
         nearestDown = catur
-  if nearestDown["color"] != rook["color"]:
-    count += 1
-  return count
+    if nearestDown["color"] != rook["color"]:
+      count += 1
   # Search vertical
+  vertical = []
+  for catur in state:
+    if catur["y"] == rook["y"] and catur["x"] != rook["x"]:
+      vertical.append(catur)
+  # get right and left
+  right = []
+  left = []
+  for catur in vertical:
+    if catur["x"] > rook["x"]:
+      right.append(catur)
+    else:
+      left.append(catur)
+  # Search nearest right
+  if len(right) > 0:
+    nearestRight = right[0]
+    for catur in right:
+      if catur["x"] - rook["x"] < nearestRight["x"] - rook["x"]:
+        nearestRight = catur
+    if nearestRight["color"] != rook["color"]:
+      count += 1
+  # Search nearest Left
+  if len(left) > 0:
+    nearestLeft = left[0]
+    for catur in left:
+      if rook["x"] - catur["x"] < rook["x"] - nearestLeft["x"]:
+        nearestLeft = catur
+    if nearestLeft["color"] != rook["color"]:
+      count += 1
+  return count
 
-  
-
-
-
-
-
-
-
-
-  
 def main():
   state = [
     {
@@ -70,6 +87,12 @@ def main():
       "x": 2,
       "y": 1,
       "color": "WHITE"
+    },
+    {
+      "type": "BISHOP",
+      "x": 6,
+      "y": 3,
+      "color": "BLACK"
     }
   ]
   printBoard(state)
