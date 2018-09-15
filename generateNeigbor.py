65e7fc04cb410201355fa8ebcf7c0faf1f071d66
haifadhila <haifadhila.ilma@gmail.com>
2018-09-15 12:55:05 +0700

    generate neighbor

---
+++ generateNeigbor.py
@@ -0,0 +1,68 @@
+import sys
+from printx import printBoard
+
+def generateNeighbor(state, neighbors):
+    neighbors = []
+    newElmt = {}
+    curstate = list(state)
+    count = 0
+    for movedElmt in curstate:
+        initx = movedElmt["x"]
+        inity = movedElmt["y"]
+        for i in range(1,9):
+            for j in range(1,9):
+                found = True
+                for elmt in state:
+                    if (i==elmt["x"]) and (j==elmt["y"]):
+                        found = False
+                if (found==True):
+                    movedElmt["x"] = i
+                    movedElmt["y"] = j
+                    neighbors.append(curstate)
+                    printBoard(curstate)
+        movedElmt["x"] = initx
+        movedElmt["y"] = inity
+    print(len(neighbors))
+
+# def evaluateCost(neighbors):
+#    for elmt in neighbors:
+def main():
+  state = [
+    {
+      "type": "ROOK",
+      "x": 2,
+      "y": 3,
+      "color": "WHITE"
+    },
+    {
+      "type": "QUEEN",
+      "x": 2,
+      "y": 5,
+      "color": "WHITE"
+    },
+    {
+      "type": "QUEEN",
+      "x": 2,
+      "y": 7,
+      "color": "WHITE"
+    },
+    {
+      "type": "QUEEN",
+      "x": 2,
+      "y": 1,
+      "color": "WHITE"
+    },
+    {
+      "type": "BISHOP",
+      "x": 6,
+      "y": 3,
+      "color": "BLACK"
+    }
+  ]
+  neighbor = []
+  printBoard(state)
+  generateNeighbor(state,neighbor)
+
+
+if __name__ == '__main__':
+  main()
