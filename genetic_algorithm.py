892fec0b45d5a2e9b3d6ff2c9dc31d6e90e6772e
timothysihombing <timothysihombing10@gmail.com>
2018-09-26 23:39:55 +0700

    genetic algorithm

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -5,71 +5,102 @@ from init import init
 import random as rand
 from copy import deepcopy
 
-def genetic_cross(states):
+def make_generation(states):
     parents = []
-    heads = []
-    tails = []
-    k = rand.randint(1, len(states)-1)
-    print("k = " + str(k))
-    print()
-
+    
     # parent initiation
     for x in range(4):
-        current = deepcopy(init(states))
+        current = {}
+        current['states'] = deepcopy(init(states))
+        current = deepcopy(insert_cost(current))
         print("PARENT " + str(x+1))
-        printBoard(current)
-        print(totalCost(current))
+        print_info(current['states'])
         parents.append(current)
-        
-    # heads initiation
-    for x in range(4):
-        heads.append(deepcopy(parents[x][:k]))
-    
-    # tail initiation
-    for x in range(4):
-        tails.append(deepcopy(parents[0][k:]))
+
+    return parents
+
+def print_info(state):
+    printBoard(state)
+    print(totalCost(state))
+
+def insert_cost(state):
+    state_cost = totalCost(state['states'])
+    state['total_diff'] = state_cost['total_diff']
+    state['total_same'] = state_cost['total_same']
+
+    return state
+
+def fitness_function(states):
+    '''
+       how to do it?
+        find the number of non-attacking pairs of elements 
+        (min = 0, max = (total element × (total element-1)))
+    '''
+    sum_pairs = 0
+    max_pairs = len(states[0]['states'])*(len(states[0]['states'])-1)
+    x = 0
+
+    # count the total of non-attacking pairs of elements
+    for state in states:
+        sum_pairs += max_pairs - (state['total_diff'] + state['total_same'])
+
+    x = 0
+    # count the fitness value of each state
+    for state in states:
+        state['fitness_value'] = (max_pairs - (state['total_diff'] + state['total_same']))/sum_pairs
+        print("FITNESS " + str(x+1))
+        print(state['fitness_value'])
+        x += 1
+
+    return states
+
+def eliminate(states):
+    sorted_states = deepcopy(sorted(states, key=lambda k: k['fitness_value'], reverse=True))
+    sorted_states.pop()
+
+    return sorted_states
+
+def genetic_cross(parents):
+    k = rand.randint(1, len(parents[0]['states'])-1)
+    print("k = " + str(k) + "\n")
 
     # cross
     children = [{},{},{},{}]
-    children[0]['states'] = deepcopy(heads[0]+tails[1])
-    children[1]['states'] = deepcopy(heads[1]+tails[0])
-    children[2]['states'] = deepcopy(heads[3]+tails[2])
-    children[3]['states'] = deepcopy(heads[3]+tails[2])
+    children[0]['states'] = deepcopy(parents[0]['states'][:k]+parents[1]['states'][k:])
+    children[1]['states'] = deepcopy(parents[1]['states'][:k]+parents[0]['states'][k:])
+    children[2]['states'] = deepcopy(parents[1]['states'])
+    children[3]['states'] = deepcopy(parents[2]['states'])
 
-    return fitness_function(children)
-
-def fitness_function(states):
-    max_diff = 0
-    max_same = 0
-    total_pairs = len(states[0]['states'])*len(states[0]['states'])
-    print(total_pairs)
     x = 0
-
-    # input the cost of each parent
-    for state in states:
+    for child in children:    
         print("PARENT AFTER CROSS " + str(x+1))
+        child = deepcopy(insert_cost(child))
+        print_info(child['states'])
         x += 1
-        printBoard(state['states'])
-        state_cost = totalCost(state['states'])
-        state['total_diff'] = state_cost['total_diff']
-        state['total_same'] = state_cost['total_same']
-        print(state_cost)
-        max_diff += state['total_diff']
-        max_same += (total_pairs - state['total_same'])
 
-    for state in states:
-        state['fitness_value'] = state['total_diff']/max_diff + state['total_same']/max_same
-        print(state['fitness_value'])
-    
-    max = 0
-    best_state = {}
-
-    for state in states:
-        if state['fitness_value'] > max:
-            max = state['fitness_value']
-            best_state = state
-
-    return best_state['states']
+    return children
 
 def genetic_algorithm(states):
-    printBoard(genetic_cross(states))
+    parents = deepcopy(make_generation(states))
+    parents = deepcopy(fitness_function(parents))
+    found = False
+    x = 1
+
+    while not(found):
+        local = 0
+        print("\n\nCROSS " + str(x))
+        parents = deepcopy(eliminate(parents))
+        children = deepcopy(genetic_cross(parents))
+        print()
+        children = deepcopy(fitness_function(children))
+        children = deepcopy(sorted(children, key=lambda k: k['fitness_value'], reverse=True))
+
+        if abs(parents[0]['fitness_value'] - children[0]['fitness_value']) < 0.1:
+            local += 1
+        elif local == 10:
+            found = True
+        else:
+            parents = deepcopy(children)
+            local = 0
+    print()
+    printBoard(parents[0]['states'])
